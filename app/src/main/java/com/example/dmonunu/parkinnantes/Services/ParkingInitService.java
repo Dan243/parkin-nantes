1eb00c1f5a1d4b6a27379977d4d31c4cdab813c6
Dan MONUNU <dan.monunu65@gmail.com>
2018-11-14 11:18:37 +0100

    Creation de la classe d'initialisation

--- app/src/main/java/com/example/dmonunu/parkinnantes/Services/ParkingInitService.java
+++ app/src/main/java/com/example/dmonunu/parkinnantes/Services/ParkingInitService.java
@@ -4,4 +4,7 @@ import com.example.dmonunu.parkinnantes.Models.ParkingProperties;
 
 public final class ParkingInitService {
 
+    public static void setParkingProperties(final ParkingProperties local, final ParkingProperties api){
+        
+    }
 }

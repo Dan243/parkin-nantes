02f782815bad81c536249bc070465b1c0b186051
Dan MONUNU <dan.monunu65@gmail.com>
2018-11-23 00:48:32 +0100

    Connexion Email et Google réussie

--- app/src/main/java/com/example/dmonunu/parkinnantes/Activities/LoginActivity.java
+++ app/src/main/java/com/example/dmonunu/parkinnantes/Activities/LoginActivity.java
@@ -1,10 +1,13 @@
 package com.example.dmonunu.parkinnantes.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.dmonunu.parkinnantes.R;
 import com.firebase.ui.auth.AuthUI;
@@ -13,28 +16,19 @@ import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
 
-    //FOR DATA
-    // 1 - Identifier for Sign-In Activity
+    @BindView(R.id.button)
+    Button mbutton;
+
     private static final int RC_SIGN_IN = 123;
 
-    // --------------------
-    // ACTIONS
-    // --------------------
-
-    @OnClick(R.id.button)
-    public void onClickLoginButton() {
-        // 3 - Launch Sign-In Activity when user clicked on Login Button
-        this.startSignInActivity();
-    }
-
-    // 2 - Launch Sign-In Activity
     private void startSignInActivity(){
         startActivityForResult(
                 AuthUI.getInstance()
                         .createSignInIntentBuilder()
                         .setTheme(R.style.LoginTheme)
                         .setAvailableProviders(
-                                Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build()))
+                                Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                        new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
                         .setIsSmartLockEnabled(false, true)
                         //.setLogo(R.drawable.ic_logo_auth)
                         .build(),
@@ -44,6 +38,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        mbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginActivity.this.startSignInActivity();
+            }
+        });
 
     }
 }

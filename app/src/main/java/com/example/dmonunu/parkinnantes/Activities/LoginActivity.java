29831ca9be47e1e886da1646dc8d8c0c8ccb4f46
Dan MONUNU <dan.monunu65@gmail.com>
2018-11-23 01:49:12 +0100

    Connexion Email, Google et Facebook réussie

--- app/src/main/java/com/example/dmonunu/parkinnantes/Activities/LoginActivity.java
+++ app/src/main/java/com/example/dmonunu/parkinnantes/Activities/LoginActivity.java
@@ -1,16 +1,21 @@
 package com.example.dmonunu.parkinnantes.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.dmonunu.parkinnantes.R;
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.Arrays;
 
@@ -19,7 +24,12 @@ public class LoginActivity extends AppCompatActivity {
     @BindView(R.id.button)
     Button mbutton;
 
+    @BindView(R.id.coordinator)
+    CoordinatorLayout coordinatorLayout;
+
+
     private static final int RC_SIGN_IN = 123;
+
 
     private void startSignInActivity(){
         startActivityForResult(
@@ -35,6 +45,37 @@ public class LoginActivity extends AppCompatActivity {
                         .build(),
                 RC_SIGN_IN);
     }
+
+
+
+
+    // 2 - Show Snack Bar with a message
+    private void showSnackBar(CoordinatorLayout coordinatorLayout, String message){
+        Snackbar.make(coordinatorLayout, message, Snackbar.LENGTH_SHORT).show();
+    }
+
+
+    private void handleResponseAfterSignIn(int requestCode, int resultCode, Intent data) {
+
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) { // SUCCESS
+                showSnackBar(this.coordinatorLayout, getString(R.string.connection_succeed));
+                Intent changePage = new Intent(this, HomeActivity.class);
+                startActivity(changePage);
+            } else { // ERRORS
+                if (response == null) {
+                    showSnackBar(this.coordinatorLayout, getString(R.string.error_authentication_canceled));
+                } else if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    showSnackBar(this.coordinatorLayout, getString(R.string.error_no_internet));
+                } else if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    showSnackBar(this.coordinatorLayout, getString(R.string.error_unknown_error));
+                }
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,5 +88,12 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // 4 - Handle SignIn Activity response on activity result
+        this.handleResponseAfterSignIn(requestCode, resultCode, data);
     }
 }

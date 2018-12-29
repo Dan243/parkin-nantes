cebfad908b5b37ae9d80666a7d0f6471b77e9e0d
Greg3883 <gregoire.bejjaji@gmail.com>
2018-12-29 15:43:32 +0100

    Resolution du probleme Utilities

--- app/src/main/java/com/example/dmonunu/parkinnantes/utilities/DrawerUtil.java
+++ app/src/main/java/com/example/dmonunu/parkinnantes/utilities/DrawerUtil.java
@@ -4,8 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.View;
 
-import com.example.dmonunu.parkinnantes.activities.ListParkingActivity;
-import com.example.dmonunu.parkinnantes.auth.ProfilActivity;
+import com.example.dmonunu.parkinnantes.activities.HomeActivity;
 import com.example.dmonunu.parkinnantes.R;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
@@ -18,25 +17,15 @@ import androidx.appcompat.widget.Toolbar;
 
 
 public class DrawerUtil {
-    public static final String LOAD_METHOD_ID = "load_method_id";
-    public static final int LOAD_METHOD_CODE = 92840;
-
-    public static final String LOAD_METHOD_ID2 = "load_method_id";
-
-    public static final int LOAD_METHOD_CODE2 = 92842;
-
     public static void getDrawer(final Activity activity, Toolbar toolbar) {
-
-
-
-
         //if you want to update the items at a later time it is recommended to keep it in a variable
-        final PrimaryDrawerItem drawerEmptyItem= new PrimaryDrawerItem().withIdentifier(0).withName("");
+        PrimaryDrawerItem drawerEmptyItem= new PrimaryDrawerItem().withIdentifier(0).withName("");
         drawerEmptyItem.withEnabled(false);
 
         PrimaryDrawerItem drawerItemManagePlayers = new PrimaryDrawerItem().withIdentifier(1)
-                .withName(R.string.manage_player).withIcon(R.drawable.outline_account_circle_24);
+                .withName(R.string.manage_player);
         PrimaryDrawerItem drawerItemManagePlayersTournaments = new PrimaryDrawerItem()
+
                 .withIdentifier(2).withName(R.string.list_parking).withIcon(R.drawable.baseline_directions_car_24);
         SecondaryDrawerItem drawerItemSettings = new SecondaryDrawerItem()
                 .withIdentifier(3).withName(R.string.favori).withIcon(R.drawable.baseline_favorite_border_24);
@@ -86,26 +75,9 @@ public class DrawerUtil {
                 .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
                     @Override
                     public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
-                        if (drawerItem.getIdentifier() == 1 && !(activity instanceof ProfilActivity)) {
+                        if (drawerItem.getIdentifier() == 2 && !(activity instanceof HomeActivity)) {
                             // load tournament screen
-                            Intent intent = new Intent(activity, ProfilActivity.class);
-                            view.getContext().startActivity(intent);
-                        }
-
-                        if (drawerItem.getIdentifier() == 2 && !(activity instanceof ListParkingActivity)) {
-                            Intent intent = new Intent(activity, ListParkingActivity.class);
-                            view.getContext().startActivity(intent);
-                        }
-
-                        if (drawerItem.getIdentifier() == 10 && !(activity instanceof ProfilActivity)){
-                            Intent intent = new Intent(activity, ProfilActivity.class);
-                            intent.putExtra(LOAD_METHOD_ID, LOAD_METHOD_CODE);
-                            view.getContext().startActivity(intent);
-                        }
-
-                        if (drawerItem.getIdentifier() == 11 && !(activity instanceof ProfilActivity)){
-                            Intent intent = new Intent(activity, ProfilActivity.class);
-                            intent.putExtra(LOAD_METHOD_ID2, LOAD_METHOD_CODE2);
+                            Intent intent = new Intent(activity, HomeActivity.class);
                             view.getContext().startActivity(intent);
                         }
                         return true;

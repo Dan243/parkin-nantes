372cf22cc364df5bd200a70d17019473e691b4e4
Greg3883 <gregoire.bejjaji@gmail.com>
2018-12-29 16:09:03 +0100

    Le commit de la der chance

--- app/src/main/java/com/example/dmonunu/parkinnantes/utilities/DrawerUtil.java
+++ app/src/main/java/com/example/dmonunu/parkinnantes/utilities/DrawerUtil.java
@@ -4,7 +4,12 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.View;
 
+<<<<<<< HEAD
 import com.example.dmonunu.parkinnantes.activities.HomeActivity;
+=======
+import com.example.dmonunu.parkinnantes.activities.ListParkingActivity;
+import com.example.dmonunu.parkinnantes.auth.ProfilActivity;
+>>>>>>> Le commit de la der chance
 import com.example.dmonunu.parkinnantes.R;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
@@ -17,20 +22,51 @@ import androidx.appcompat.widget.Toolbar;
 
 
 public class DrawerUtil {
+    public static final String LOAD_METHOD_ID = "load_method_id";
+    public static final int LOAD_METHOD_CODE = 92840;
+
+    public static final String LOAD_METHOD_ID2 = "load_method_id";
+
+    public static final int LOAD_METHOD_CODE2 = 92842;
+
     public static void getDrawer(final Activity activity, Toolbar toolbar) {
+
+
+
+
         //if you want to update the items at a later time it is recommended to keep it in a variable
-        PrimaryDrawerItem drawerEmptyItem= new PrimaryDrawerItem().withIdentifier(0).withName("");
+        final PrimaryDrawerItem drawerEmptyItem= new PrimaryDrawerItem().withIdentifier(0).withName("");
         drawerEmptyItem.withEnabled(false);
 
         PrimaryDrawerItem drawerItemManagePlayers = new PrimaryDrawerItem().withIdentifier(1)
-                .withName(R.string.manage_player);
+                .withName(R.string.manage_player).withIcon(R.drawable.outline_account_circle_24);
         PrimaryDrawerItem drawerItemManagePlayersTournaments = new PrimaryDrawerItem()
+<<<<<<< HEAD
 
                 .withIdentifier(2).withName(R.string.list_parking).withIcon(R.drawable.baseline_directions_car_24);
         SecondaryDrawerItem drawerItemSettings = new SecondaryDrawerItem()
                 .withIdentifier(3).withName(R.string.favori).withIcon(R.drawable.baseline_favorite_border_24);
         SecondaryDrawerItem drawerItemAbout = new SecondaryDrawerItem().withIdentifier(4)
                 .withName(R.string.rechercher).withIcon(R.drawable.baseline_search_24);
+=======
+                .withIdentifier(2).withName(R.string.list_parking).withIcon(R.drawable.baseline_directions_car_24);
+
+
+        SecondaryDrawerItem drawerItemSettings = new SecondaryDrawerItem().withIdentifier(3);
+
+        SecondaryDrawerItem drawerItemAbout = new SecondaryDrawerItem().withIdentifier(4);
+        SecondaryDrawerItem drawerItemAbout1 = new SecondaryDrawerItem().withIdentifier(5);
+        SecondaryDrawerItem drawerItemAbout2 = new SecondaryDrawerItem().withIdentifier(6);
+        SecondaryDrawerItem drawerItemAbout3 = new SecondaryDrawerItem().withIdentifier(7);
+        SecondaryDrawerItem drawerItemAbout4 = new SecondaryDrawerItem().withIdentifier(8);
+        SecondaryDrawerItem drawerItemAbout5 = new SecondaryDrawerItem().withIdentifier(9);
+        SecondaryDrawerItem drawerItemDelete = new SecondaryDrawerItem().withIdentifier(10)
+                .withName("Supprimer mon compte").withIcon(R.drawable.outline_delete_forever_24);
+        SecondaryDrawerItem drawerItemDeconnexion = new SecondaryDrawerItem().withIdentifier(11)
+                .withName("Se déconnecter").withIcon(R.drawable.outline_exit_to_app_24);
+
+
+>>>>>>> Le commit de la der chance
 
         SecondaryDrawerItem drawerItemAbout1 = new SecondaryDrawerItem().withIdentifier(5);
         SecondaryDrawerItem drawerItemAbout2 = new SecondaryDrawerItem().withIdentifier(6);
@@ -59,6 +95,7 @@ public class DrawerUtil {
                         drawerItemManagePlayersTournaments,
                         new DividerDrawerItem(),
                         drawerItemAbout,
+<<<<<<< HEAD
                         new DividerDrawerItem(),
                         drawerItemSettings,
                         new DividerDrawerItem(),
@@ -69,15 +106,41 @@ public class DrawerUtil {
                         drawerItemAbout5,
 
                         new DividerDrawerItem(),
+=======
+                        drawerItemAbout1,
+                        drawerItemAbout2,
+                        drawerItemAbout3,
+                        drawerItemAbout4,
+                        drawerItemAbout5,
+                        drawerItemSettings,
+                        new DividerDrawerItem(),
+>>>>>>> Le commit de la der chance
                         drawerItemDelete,
                         drawerItemDeconnexion
                 )
                 .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
                     @Override
                     public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
-                        if (drawerItem.getIdentifier() == 2 && !(activity instanceof HomeActivity)) {
+                        if (drawerItem.getIdentifier() == 1 && !(activity instanceof ProfilActivity)) {
                             // load tournament screen
-                            Intent intent = new Intent(activity, HomeActivity.class);
+                            Intent intent = new Intent(activity, ProfilActivity.class);
+                            view.getContext().startActivity(intent);
+                        }
+
+                        if (drawerItem.getIdentifier() == 2 && !(activity instanceof ListParkingActivity)) {
+                            Intent intent = new Intent(activity, ListParkingActivity.class);
+                            view.getContext().startActivity(intent);
+                        }
+
+                        if (drawerItem.getIdentifier() == 10 && !(activity instanceof ProfilActivity)){
+                            Intent intent = new Intent(activity, ProfilActivity.class);
+                            intent.putExtra(LOAD_METHOD_ID, LOAD_METHOD_CODE);
+                            view.getContext().startActivity(intent);
+                        }
+
+                        if (drawerItem.getIdentifier() == 11 && !(activity instanceof ProfilActivity)){
+                            Intent intent = new Intent(activity, ProfilActivity.class);
+                            intent.putExtra(LOAD_METHOD_ID2, LOAD_METHOD_CODE2);
                             view.getContext().startActivity(intent);
                         }
                         return true;

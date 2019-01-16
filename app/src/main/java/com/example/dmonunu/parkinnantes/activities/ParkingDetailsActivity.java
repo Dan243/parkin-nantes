0b08e5e5dd8bb35c6eca35b08e92f2e80983ff70
Dan MONUNU <dan.monunu65@gmail.com>
2019-01-16 09:28:20 +0100

    Parking detail activity done

--- app/src/main/java/com/example/dmonunu/parkinnantes/activities/ParkingDetailsActivity.java
+++ app/src/main/java/com/example/dmonunu/parkinnantes/activities/ParkingDetailsActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,6 +15,8 @@ import android.widget.Toolbar;
 
 import com.example.dmonunu.parkinnantes.R;
 import com.example.dmonunu.parkinnantes.models.LightParking;
+import com.example.dmonunu.parkinnantes.models.ParkingDataBase;
+import com.example.dmonunu.parkinnantes.ui.MyAdapter;
 import com.example.dmonunu.parkinnantes.utilities.DrawerUtil;
 import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
 import com.google.android.gms.maps.StreetViewPanorama;
@@ -43,9 +48,6 @@ public class ParkingDetailsActivity extends AppCompatActivity implements OnStree
 
     @BindView(R.id.parking_details_accessible_places)
     TextView parkingaccessible;
-/*
-    @BindView(R.id.parking_details_favorites_button)
-    ImageButton parkingFavButton;
 
     @BindView(R.id.credit_card_imageview)
     ImageView creditCardImageView;
@@ -70,9 +72,18 @@ public class ParkingDetailsActivity extends AppCompatActivity implements OnStree
 
     @BindView(R.id.parking_details_ligne4_imageview)
     ImageView ligne4Img;
-*/
-    LightParking selectedParking;
 
+    @BindView(R.id.parking_details_favorites_button)
+    ImageView favorite;
+
+    @BindView(R.id.parking_telephone)
+    TextView telephone;
+
+    @BindView(R.id.parking_website)
+    TextView website;
+
+    LightParking selectedParking;
+    private Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -81,15 +92,47 @@ public class ParkingDetailsActivity extends AppCompatActivity implements OnStree
 
         toolbar.setTitle("Détail du parking");
         DrawerUtil.getDrawer(this,toolbar);
+        this.context = this.getApplicationContext();
         final StreetViewPanoramaFragment streetViewPanoramaFragment =
                 (StreetViewPanoramaFragment) getFragmentManager().findFragmentById(R.id
                         .parking_details_mapView);
         streetViewPanoramaFragment.getStreetViewPanoramaAsync(this);
 
         this.selectedParking = (LightParking) getIntent().getSerializableExtra("SelectedParking");
-  //      this.setPublicsTransports();
-  //      this.setPaymentOptions();
+        this.setPublicsTransports();
+        this.setPaymentOptions();
         this.setTextViewFields();
+
+
+        if (!selectedParking.isFavorite()){
+            favorite.setBackgroundResource(R.drawable.star_grey);
+            favorite.setTag(R.drawable.star_grey);
+        } else {
+            favorite.setBackgroundResource(R.drawable.star_yellow);
+            favorite.setTag(R.drawable.star_yellow);
+        }
+        favorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer fTag = (Integer) favorite.getTag();
+                fTag = fTag == null ? R.drawable.star_grey : fTag;
+                switch(fTag) {
+                    case R.drawable.star_grey:
+                        selectedParking.setFavorite(true);
+                        favorite.setBackgroundResource(R.drawable.star_yellow);
+                        favorite.setTag(R.drawable.star_yellow);
+                        new ParkingDetailsActivity.RoomFavoriTrueAsyncTask().execute(selectedParking);
+                        break;
+                    default:
+                        selectedParking.setFavorite(false);
+                        favorite.setBackgroundResource(R.drawable.star_grey);
+                        favorite.setTag(R.drawable.star_grey);
+                        new ParkingDetailsActivity.RoomFavoriFalseAsyncTask().execute(selectedParking);
+                        break;
+                }
+            }
+        });
+
     }
 
     @Override
@@ -107,8 +150,10 @@ public class ParkingDetailsActivity extends AppCompatActivity implements OnStree
         this.presentation.setText(this.selectedParking.getPresentation());
         this.parkingmoto.setText(this.selectedParking.getCapacite_moto() + "");
         this.parkingaccessible.setText(this.selectedParking.getCapacite_pmr() + "");
+        this.telephone.setText(this.selectedParking.getTelephone());
+        this.website.setText("http://www.parkings-nantes.fr");
     }
-/*
+
     private void setPaymentOptions() {
         if (!this.selectedParking.isCreditCardAvailable()) {
             this.creditCardImageView.setVisibility(ImageView.GONE);
@@ -138,5 +183,21 @@ public class ParkingDetailsActivity extends AppCompatActivity implements OnStree
             this.ligne4Img.setVisibility(ImageView.GONE);
         }
     }
-    */
+
+    private class RoomFavoriTrueAsyncTask extends AsyncTask<LightParking, Void, Void> {
+        @Override
+        protected Void doInBackground(LightParking... lightParkings) {
+            ParkingDataBase.getAppDatabase(context).lightParkingDao().setFavorite(lightParkings[0].getIdobj(), true);
+            return null;
+        }
+    }
+
+    private class RoomFavoriFalseAsyncTask extends AsyncTask<LightParking, Void, Void> {
+        @Override
+        protected Void doInBackground(LightParking... lightParkings) {
+            ParkingDataBase.getAppDatabase(context).lightParkingDao().setFavorite(lightParkings[0].getIdobj(), false);
+            return null;
+        }
+    }
+
 }

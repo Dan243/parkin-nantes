4bde3c19e03d5f08b4d773f6ed8825486ed86078
zyxie <zyxie1995@gmail.com>
2019-01-20 22:55:36 +0100

    [Android]Move ParkingPrenter to package service

---
+++ app/src/main/java/com/example/dmonunu/parkinnantes/services/ParkingPresenter.java
@@ -0,0 +1,20 @@
+package com.example.dmonunu.parkinnantes.services;
+
+import com.example.dmonunu.parkinnantes.models.LightParking;
+
+import java.util.List;
+
+public interface ParkingPresenter {
+
+    void getParkings();
+
+    void getParkingsFromApi();
+
+    boolean isNetworkOnline();
+
+    void getParkingsFromRoom();
+
+    void getFavoriteParkings();
+
+    LightParking updateParkingFavori(LightParking parking, List<LightParking> parkings);
+}
